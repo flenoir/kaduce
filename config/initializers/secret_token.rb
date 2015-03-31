cfc04eca90bffa28c0de8441c2d9c18364325bc3
Fabien Lenoir <flenoir@gmail.com>
2015-03-31 22:54:00 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaduce::Application.config.secret_key_base = 'efbe41664d033cd63c9bf1f875043404b2d8d8bb43a766db850e0c9802bb059a68e2ded3364985d29132995f646d35bbca29b18c083980f53fd5ebf07c5e8653'
